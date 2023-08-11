ef86d74c69ef7d34cc9befba03e559ab133fdfd2
Bisratke <bisratkesenay@gmail.com>
2023-08-11 04:25:53 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*main :print alphabets in small case*/
+/**
+* main - Prints a random number and states whether
+*it is positive, negative, or zero.
+* Return: Always 0.
+*/
 int main(void)
 {
 char s = 'a';
