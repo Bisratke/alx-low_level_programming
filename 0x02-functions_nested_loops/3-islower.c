f7ccf34f49bc6b1582e11a136661799b3b155c63
Bisratke <bisratkesenay@gmail.com>
2023-08-15 15:50:10 +0300

    3-main.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "3-main.c "
+/**
+* _islower - Return 1 if letter is lowercase, 0 if not.
+* Return: 1 if lowercase, 0 if not lowercase
+*/
+int _islower(int c)
+{
+if (x > 64 && x < 91)
+return (0);
+else
+return (1);
+}
