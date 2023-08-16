c87c01338168d5661f1e9196a7e41dfa96934c4c
bisratkesenay@gmail.com <bisratkesenay@gmail.com>
2023-08-16 09:08:02 +0300

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "5-main.c"
+/**
+* print_sign - Print the sign of a number
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
