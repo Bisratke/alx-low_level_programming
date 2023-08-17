6392e799f3d92d9b9a5373a927c316291fd3667c
Bisratke <bisratkesenay@gmail.com>
2023-08-17 04:28:51 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,10 +1,15 @@
 #include "main.h"
+/**
+ * main - chek number greater than zero or lessthan or equal
+ * Return :0 succes
+ */
 void positive_or_negative(int i)
 {
-if (i > 0)
+if(i > 0)
 printf("%d is positive\n", i);
 else if (i < 0)
 printf("%d is negative\n", i);
 else
 printf("%d is zero\n", i);
+return;
 }
