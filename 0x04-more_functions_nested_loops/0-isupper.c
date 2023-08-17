d30f220da41cb0ee7a306eb9d571c50aceddcba1
Bisratke <bisratkesenay@gmail.com>
2023-08-17 06:52:45 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isupper - function that checks for uppercase characters
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
+ */
+int _isupper(int c)
+{
+if  (isuper(c))
+return (1);
+
+return (0);
+}
