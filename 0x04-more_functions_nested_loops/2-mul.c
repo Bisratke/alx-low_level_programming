37a3c200b2fbec0e8fa3ee730cdea7e5c2a2ab2a
Bisratke <bisratkesenay@gmail.com>
2023-08-18 13:09:21 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multipintiger a by intiger b
+ * @a: first int
+ * @b: second int
+ * Return: result of the multiplicattion
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
