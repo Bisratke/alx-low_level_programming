5650ad4c0c248b3ff39de9a23880e651508a9a4b
Bisratke <bisratkesenay@gmail.com>
2023-08-24 05:50:29 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,25 @@
 #include "main.h"
 
 /**
- * leet - Encodes a string into 1337
- * @c: String of variable pointer
- * Return: String that is encoded
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
  */
-
-char *leet(char *c)
+char *leet(char *s)
 {
-char *cp = c;
-char key[] = {'A', 'E', 'O', 'T', 'L'};
-int value[] = {4, 3, 0, 7, 1};
-unsigned int i = 0;
-while (*c)
+int i;
+int j = 0;
+char a[] = "aAeEoOtTlL";
+char b[] = "4433007711";
+for (i = 0; *(s + i); i++)
 {
-while (i < sizeof(key) / sizeof(char))
+while (j <= 9)
 {
-/*32 is the difference between lower case letters and apper case letters*/
-if (*c == key[i] || *c == key[i] + 32)
-{
-*c = 48 + value[i];
-}
+if (a[j] == *(s + i))
+*(s + i) = b[j];
 i++;
 }
-c++;
 }
-return (cp);
+return (s);
 }
