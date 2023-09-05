0682a5cfd13a310c6c71a62ba5bf3d1983b89b3e
Bisratke <bisratkesenay@gmail.com>
2023-09-05 01:08:17 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+while (i < argc)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}
