09f955106e19a05f4ba346f0da5c357f1bcf1d1f
Bisratke <bisratkesenay@gmail.com>
2023-09-04 23:29:04 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,26 +10,27 @@
 
 char *str_concat(char *s1, char *s2)
 {
-char *a;
-int i, j, c, d;
+char *strout;
+unsigned int i = 0, j, k, limit;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (i = 0; s1[i] != '\0'; i++)
+while (s1[i] != '\0')
+i++;
 ;
 for (j = 0; s2[j] != '\0'; j++)
 ;
-a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
-if (a == NULL)
-free(a);
-return (NULL);
-for (c = 0, d = 0; c < (i + j + 1); c++)
+strout = malloc(sizeof(char) * (i + j + 1));
+if (strout == NULL)
 {
-if (c < i)
-a[c] = s1[c];
-else
-a[c] = s2[d++];
+free(strout);
+return (NULL);
 }
-return (a);
+for (k = 0; k < i; k++)
+strout[k] = s1[k];
+limit = j;
+for (j = 0; j <= limit; k++, j++)
+strout[k] = s2[j];
+return (strout);
 }
