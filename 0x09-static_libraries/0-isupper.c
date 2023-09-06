b17365f40888a768387b67f370f817b15d37edf1
Bisratke <bisratkesenay@gmail.com>
2023-09-06 11:59:13 -0700

    all static library

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-* _islower - Return 1 if letter is lowercase, 0 if not.
+* _isupper - Return 1 if letter is lowercase, 0 if not.
 * Return: 1 if Isupper, 0 if  lowercase
+* @c: The character to print
 */
-int _isuppe(int c)
+int _isupper(int c)
 {
 if (c > 64 && c < 91)
 return (1);
