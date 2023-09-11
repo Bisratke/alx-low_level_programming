22ce10351e9d00c64508a0ce4e29c516558d5e14
Bisratke <bisratkesenay@gmail.com>
2023-09-11 02:11:10 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,9 @@
  * @owner: string for owners name
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
+{i
+if (d == NULL)
+d = malloc(sizeof(struct dog));
 d->name = name;
 d->age = age;
 d->owner = owner;
