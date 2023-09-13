cd7e5e0f1a55c66cb90274b0eb1d7e356d57a93b
Bisratke <bisratkesenay@gmail.com>
2023-09-13 02:11:13 -0700

    updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,12 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - selects the correct function to perform
- * the operation asked by the user.
- * @s: char operator.
- *
- * Return: pointer to the function that corresponds to the operator.
+ * get_op_func - should contain the function that selects the correct
+ * function to perform the operation asked by the use
+ * @s: struct s (int, int)
+ * Return: NULL
  */
+
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
@@ -18,10 +18,10 @@ op_t ops[] = {
 {NULL, NULL}
 };
 int i;
-for (i = 0; i < 10; i++)
+for (i = 0; i < 5; i++)
 {
-if (s[0] == ops->op[i])
-break;
+if (ops[i].op[0] == s[0])
+return (ops[i].f);
 }
-return (ops[i / 2].f);
+return (NULL);
 }
