eeeef4d49c42d0989d7d7879033e2596469f35ed
Bisratke <bisratkesenay@gmail.com>
2023-09-13 01:03:50 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+char *opc = (char *) main;
+int i, nbytes;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+nbytes = atoi(argv[1]);
+if (nbytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+i = 0;
+while (i < nbytes)
+{
+printf("%02x", opc[i] & 0xFF);
+if (i != nbytes - 1)
+printf(" ");
+i++;
+}
+printf("\n");
+return (0);
+}
