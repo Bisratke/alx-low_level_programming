4b9f90ac973ecd52d6f274f67dcf6fd601e36567
Bisratke <Bisratkesenay@gmail.com>
2023-10-31 11:39:15 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int i, file;
+int i = 0, file;
 if (!filename)
 return (-1);
 if (!text_content)
 text_content = "";
-for  (i = 0; text_content[i] != '\0'; i++)
+while (text_content[i] != '\0')
+{
+i++;
+}
 file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (file == -1)
 return (-1);
